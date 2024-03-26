6a3ae9761687e8abe9d82a647a5a74f6393b5509
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-26 23:46:56 +0300

    add hero image and hero text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,20 @@
 import "./hero.css";
 function Hero() {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <div className="heroContainer">
+        <div className="textContainer">
+          <h1>
+            The Best <br /> Delicious Food
+          </h1>
+          <p>That meets your needs</p>
+        </div>
+        <div className="imageContainer">
+          <img src="/foodhero.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
