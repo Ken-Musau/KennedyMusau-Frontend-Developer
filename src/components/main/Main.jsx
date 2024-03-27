e9b00969371af0113086d5c40e421a47ef9f8513
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-27 19:31:24 +0300

    add and style sort buttons

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,26 @@
 import "./main.css";
+
+import { IoFilterOutline } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa";
+
 function Main() {
   return (
     <div className="main">
       <div className="mainContainer">
         <div className="mainheading">
           <h2>Restaurants with online food delivery</h2>
+        </div>
+
+        <div className="sort-btns">
+          <button className="btn">
+            Filter <IoFilterOutline />
+          </button>
+          <button className="btn">
+            Sorty By <FaAngleDown />
+          </button>
+          <button className="btn">Fast Delivery</button>
+          <button className="btn">New on Feasty</button>
+          <button className="btn">Offers</button>
         </div>
       </div>
     </div>
